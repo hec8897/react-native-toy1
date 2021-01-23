3138b81dcc4bb107e713fb0fb1db27e7f69abcbd
hec8897 <hec8897@naver.com>
2021-01-23 19:06:55 +0900

    버튼 예제

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {  Image, StyleSheet, Text, View } from 'react-native';
+import {  Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import logo from './assets/logo.png'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
+      
       <Image source={logo} style={styles.logo} /> 
 
       <Text style={styles.instructions}>Hi up App.js to start working on your app!</Text>
+
+      <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -29,6 +36,16 @@ const styles = StyleSheet.create({
   instructions: {
     color: '#888',
     fontSize: 24,
+    marginBottom: 20,
     marginHorizontal: 15,
-  }
+  },
+  button: {
+    backgroundColor: "skyblue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
 });
