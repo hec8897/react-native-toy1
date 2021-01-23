cbd7dcca7cbbce9d54d10384c28914d081b93e60
hec8897 <hec8897@naver.com>
2021-01-23 19:13:32 +0900

    이미지 선택 출력

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import * as ImagePicker from 'expo-image-picker';
 import logo from './assets/logo.png'; 
 
 export default function App() {
-  
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
   let openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
 
@@ -16,7 +17,25 @@ export default function App() {
 
     let pickerResult = await ImagePicker.launchImageLibraryAsync();
     console.log(pickerResult);
-  }
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+    
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -62,4 +81,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#fff',
   }, 
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
 });
